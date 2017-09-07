5f16e8329698418174e0d709c6ae0557859eaddf
Elizabeth Lei <beth.a.lei@gmail.com>
2017-09-06 22:57:40 -0400

    add search book component

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Book from './Book'
+import { search } from './BooksAPI';
 
 class SearchBook extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelves: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: '',
+    queryBooks: []
+  }
+
+  searchBooks(query) {
+    this.setState({ query: query.trim() })
+
+    search(query, 20).then((books) => {
+      if (books) {
+        this.setState({ queryBooks: books })
+      } else {
+        this.setState({ queryBooks: [] })
+      }
+    })
+  }
 
   render() {
+    const { query, queryBooks } = this.state
+    const { shelves, onChangeShelf } = this.props
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -18,12 +45,32 @@ class SearchBook extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(e) => this.searchBooks(e.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {queryBooks && queryBooks.map(book => {
+              const { id, authors, imageLinks, shelf, title } = book
+              return (
+                <li key={id}>
+                  <Book
+                    authors={authors}
+                    id={id}
+                    imageLinks={imageLinks}
+                    title={title}
+                    shelf={shelf}
+                    shelves={shelves}
+                    onChangeShelf={onChangeShelf}
+                    />
+                </li>
+              )
+            })}
+          </ol>
         </div>
       </div>
     )
