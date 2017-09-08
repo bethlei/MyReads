b7a105f56be9224c040f3b3046be588c9f2eac45
Elizabeth Lei <beth.a.lei@gmail.com>
2017-09-08 15:48:13 -0400

    refactor ListBooks to a stateless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import BookShelf from './BookShelf'
 import { shelfTitle, booksByShelf } from './utils'
 
-class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    shelves: PropTypes.array.isRequired,
-    onChangeShelf: PropTypes.func.isRequired
-  }
-
-  render() {
-    const { books, shelves, onChangeShelf } = this.props
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          {shelves.map(shelf => (
-            <div key={shelf} className="bookshelf">
-              <h2 className="bookshelf-title">{shelfTitle(shelf)}</h2>
-              <div className="bookshelf-books">
-                <BookShelf
-                  books={booksByShelf(books, shelf)}
-                  shelves={shelves}
-                  onChangeShelf={onChangeShelf} />
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="open-search">
-          <Link to ='/search' className='add-contact'>Add Contact</Link>
-        </div>
+const ListBooks = (props) => {
+  const { books, shelves, onChangeShelf } = props
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
+      <div className="list-books-content">
+        {shelves.map(shelf => (
+          <div key={shelf} className="bookshelf">
+            <h2 className="bookshelf-title">{shelfTitle(shelf)}</h2>
+            <div className="bookshelf-books">
+              <BookShelf
+                books={booksByShelf(books, shelf)}
+                shelves={shelves}
+                onChangeShelf={onChangeShelf} />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="open-search">
+        <Link to ='/search' className='add-contact'>Add Contact</Link>
+      </div>
+    </div>
+  )
+}
+
+ListBooks.propTypes = {
+  books: PropTypes.array.isRequired,
+  shelves: PropTypes.array.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
 }
 
 export default ListBooks;
